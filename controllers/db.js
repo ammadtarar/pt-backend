2f54f276862606547c5ecb129297119ec89cbe31
Ammad Amjad Tarar <ammadtarar@qq.com>
2021-01-28 17:19:10 +0500

    Updated DB object

--- controllers/db.js
+++ controllers/db.js
@@ -6,10 +6,10 @@ var sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
-        storage: process.env.DB_FILE_PATH,
-        operatorsAliases: false,
-        logging: false
+        port: process.env.DB_PORT,
+        logging: true
     });
 
 var db = {};
