a7c4883fad0b4d6f2a9e5f9d593ff585793d3da2
Ammad Amjad Tarar <ammadtarar@qq.com>
2021-01-29 17:55:12 +0500

    Updated

--- controllers/db.js
+++ controllers/db.js
@@ -1,16 +1,32 @@
 require('custom-env').env(true)
 const Sequelize = require('sequelize');
+const { lastIndexOf } = require('underscore');
 
 
-var sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
-        logging: true
-    });
+var sequelize;
+console.log("process.env.NODE_ENV = ", process.env.NODE_ENV);
+if (process.env.NODE_ENV == 'development') {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD, {
+            dialect: process.env.DB_DIALECT,
+            storage: process.env.DB_FILE_PATH,
+            operatorsAliases: false,
+            logging: false
+        });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD, {
+            host: process.env.DB_HOST,
+            dialect: process.env.DB_DIALECT,
+            port: process.env.DB_PORT,
+            logging: true
+        });
+}
+
 
 var db = {};
 
