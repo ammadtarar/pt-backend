54ac9b3420d9c28981375c928ab653c91dac6d41
Ammad Amjad Tarar <info@sawatechnologies.org>
2021-07-03 11:23:29 +0500

    Integrated sentry.io for bug tracking

--- endpoints/super_admin/routes/index.js
+++ endpoints/super_admin/routes/index.js
@@ -1,110 +1,127 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const app = Router();
-const underscore = require('underscore');
-const db = require('../../../controllers/db.js');
-const middleware = require('../../../controllers/middleware.js')(db);
+const underscore = require("underscore");
+const db = require("../../../controllers/db.js");
+const middleware = require("../../../controllers/middleware.js")(db);
 
-app.post('/register', middleware.authenticateSuperAdmin, (req, res, next) => {
-    let body = underscore.pick(req.body, 'email', "name", "password");
-    db.super_admin.create(body)
-        .then((user) => {
-            res.json({
-                message: res.__('admin_created'),
-                user: user.toPublicJSON()
-            });
-        })
-        .catch((err) => {
-            next(err);
-        });
+app.post("/register", middleware.authenticateSuperAdmin, (req, res, next) => {
+  let body = underscore.pick(req.body, "email", "name", "password");
+  db.super_admin
+    .create(body)
+    .then((user) => {
+      res.json({
+        message: res.__("admin_created"),
+        user: user.toPublicJSON(),
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
-app.post('/login', (req, res, next) => {
-    const body = underscore.pick(req.body, 'email', 'password');
-    var token, userInstance;
-    db.super_admin.authenticate(body)
-        .then((user) => {
-            userInstance = user;
-            token = user.generateToken('authentication');
-            return db.super_admin.update({
-                token: token
-            }, {
-                where: {
-                    id: user.id
-                }
-            });
-        })
-        .then((u) => {
-            res.header('Authentication', token)
-            var user = userInstance.toPublicJSON();
-            user.token = token
-            res.json({
-                message: res.__('login_success'),
-                user: user
-            })
-        })
-        .catch((e) => {
-            next(e);
-        });
+app.post("/login", (req, res, next) => {
+  const body = underscore.pick(req.body, "email", "password");
+  var token, userInstance;
+  db.super_admin
+    .authenticate(body)
+    .then((user) => {
+      userInstance = user;
+      token = user.generateToken("authentication");
+      return db.super_admin.update(
+        {
+          token: token,
+        },
+        {
+          where: {
+            id: user.id,
+          },
+        }
+      );
+    })
+    .then((u) => {
+      res.header("Authentication", token);
+      var user = userInstance.toPublicJSON();
+      user.token = token;
+      res.json({
+        message: res.__("login_success"),
+        user: user,
+      });
+    })
+    .catch((e) => {
+      next(e);
+    });
 });
 
-app.patch('/change_password', middleware.authenticateSuperAdmin, (req, res, next) => {
-    const body = underscore.pick(req.body, 'new_password');
-    if (body === null || body === undefined || Object.keys(body).length === 0 || body.new_password === null || body.new_password === undefined || body.new_password.length <= 0) {
-        res.status(422).send({
-            message: res.__('new_pwd_missing')
-        });
-        return
+app.patch(
+  "/change_password",
+  middleware.authenticateSuperAdmin,
+  (req, res, next) => {
+    const body = underscore.pick(req.body, "new_password");
+    if (
+      body === null ||
+      body === undefined ||
+      Object.keys(body).length === 0 ||
+      body.new_password === null ||
+      body.new_password === undefined ||
+      body.new_password.length <= 0
+    ) {
+      res.status(422).send({
+        message: res.__("new_pwd_missing"),
+      });
+      return;
     }
 
-    db.super_admin.update({
-            password: body.new_password
-        }, {
-            where: {
-                id: req.user.id
-            }
-        })
-        .then((status) => {
-            if (status) {
-                res.json({
-                    message: res.__('pwd_change_success')
-                });
-            } else {
-                res.json({
-                    message: res.__('pwd_change_fail')
-                });
-            }
-        })
-        .catch((err) => {
-            next(err);
-        });
+    db.super_admin
+      .update(
+        {
+          password: body.new_password,
+        },
+        {
+          where: {
+            id: req.user.id,
+          },
+        }
+      )
+      .then((status) => {
+        if (status) {
+          res.json({
+            message: res.__("pwd_change_success"),
+          });
+        } else {
+          res.json({
+            message: res.__("pwd_change_fail"),
+          });
+        }
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+);
 
-});
+app.get("/list/all", middleware.authenticateSuperAdmin, (req, res, next) => {
+  var limit = parseInt(req.query.limit) || 10;
+  var page = parseInt(req.query.page) || 0;
+  if (page >= 1) {
+    page = page - 1;
+  }
 
-app.get('/list/all', middleware.authenticateSuperAdmin, (req, res, next) => {
-    var limit = parseInt(req.query.limit) || 10;
-    var page = parseInt(req.query.page) || 0;
-    if (page >= 1) {
-        page = page - 1;
-    }
-
-
-    db.super_admin.findAndCountAll({
-            limit: limit,
-            offset: limit * page,
-            order: [
-                ['createdAt', 'DESC']
-            ],
-            attributes: {
-                exclude: ['salt', 'password_hash', 'tokenHash']
-            },
-            distinct:true
-        })
-        .then((super_admins) => {
-            res.json(super_admins);
-        })
-        .catch((err) => {
-            next(err);
-        })
+  db.super_admin
+    .findAndCountAll({
+      limit: limit,
+      offset: limit * page,
+      order: [["createdAt", "DESC"]],
+      attributes: {
+        exclude: ["salt", "password_hash", "tokenHash"],
+      },
+      distinct: true,
+    })
+    .then((super_admins) => {
+      res.json(super_admins);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,35 +1,82 @@
-const express = require('express');
-const db = require('../controllers/db.js');
-const middleware = require('../controllers/middleware.js')(db);
+const express = require("express");
 
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 const server = express();
 
-let cors = require('cors')
+// or using CommonJS
+// const express = require('express');
+// const Sentry = require('@sentry/node');
+// const Tracing = require("@sentry/tracing");
 
+Sentry.init({
+  dsn: "https://73e2868bd45d45998d92de0541acbd7d@o910721.ingest.sentry.io/5845631",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ server }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+server.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+server.use(Sentry.Handlers.tracingHandler());
+
+// All controllers should live here
+server.get("/", function rootHandler(req, res) {
+  res.end("Hello world!");
+});
+
+// The error handler must be before any other error middleware and after all controllers
+server.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+server.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
+server.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+const db = require("../controllers/db.js");
+const middleware = require("../controllers/middleware.js")(db);
+
+let cors = require("cors");
 server.use(cors());
 
 server.use(express.json());
 server.use(express.urlencoded());
 server.use(middleware.logger);
 
-const { I18n } = require('i18n');
+const { I18n } = require("i18n");
 const i18n = new I18n();
 i18n.configure({
-    staticCatalog: {
-        en: require('../locales/en.json'),
-        fr: require('../locales/fr.json')
-    },
-    defaultLocale: 'en'
-})
+  staticCatalog: {
+    en: require("../locales/en.json"),
+    fr: require("../locales/fr.json"),
+  },
+  defaultLocale: "en",
+});
 
-server.use(i18n.init)
+server.use(i18n.init);
 
 //ROUTES
-server.use('/super_admin/', require('../endpoints/super_admin/routes'));
-server.use('/company/', require('../endpoints/company'));
-server.use('/dashbaord', require('../endpoints/dashboard/routes'));
-server.use('/settings', require('../endpoints/settings/routes'));
-
+server.use("/super_admin/", require("../endpoints/super_admin/routes"));
+server.use("/company/", require("../endpoints/company"));
+server.use("/dashbaord", require("../endpoints/dashboard/routes"));
+server.use("/settings", require("../endpoints/settings/routes"));
 
 server.use(middleware.errorHandler);
 
